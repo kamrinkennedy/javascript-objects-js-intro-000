4a84781caca05e07feef9a858a53f96135c172b1
Kamrin Kennedy <kamrinkennedy@gmail.com>
2018-10-06 16:43:37 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -9,10 +9,10 @@ var playlist = {
   'Robyn' : 'Dancing on My Own'
 }
 
-// function updatePlaylist(playlist, artistName, songTitle) {
-//   return playlist.[artistName] = songTitle
-// }
+function updatePlaylist(playlist, artistName, songTitle) {
+  return playlist.[artistName] = songTitle
+}
 
-// function removeFromPlaylist(playlist, artistName) {
-//   return delete playlist.[artistName]
-// }
+function removeFromPlaylist(playlist, artistName) {
+  return delete playlist.[artistName]
+}
