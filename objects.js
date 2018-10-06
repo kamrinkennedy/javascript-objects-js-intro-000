fe80998bd57d2b35dda052b1cdcb186af0994153
Kamrin Kennedy <kamrinkennedy@gmail.com>
2018-10-06 16:43:17 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -1,12 +1,12 @@
 var playlist = {
-  Beyonce : 'Formation',
-  Lady Gaga : 'Million Reasons',
-  Billy Joel : 'Always a Woman',
-  Train : 'Drops of Jupiter',
-  Fleetwood Mac : 'Silver Springs',
-  Sam Smith : 'Too Good at Goodbyes',
-  Lizzo : 'Truth Hurts',
-  Robyn : 'Dancing on My Own'
+  'Beyonce' : 'Formation',
+  'Lady Gaga' : 'Million Reasons',
+  'Billy Joel' : 'Always a Woman',
+  'Train' : 'Drops of Jupiter',
+  'Fleetwood Mac' : 'Silver Springs',
+  'Sam Smith' : 'Too Good at Goodbyes',
+  'Lizzo' : 'Truth Hurts',
+  'Robyn' : 'Dancing on My Own'
 }
 
 // function updatePlaylist(playlist, artistName, songTitle) {
