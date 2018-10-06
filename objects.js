c2af56e7ba56140c43214c833354aa9ad92fb6e1
Kamrin Kennedy <kamrinkennedy@gmail.com>
2018-10-06 16:36:49 +0000

    automatically backed up by learn

---
+++ objects.js
@@ -0,0 +1,10 @@
+var playlist = {
+  Beyonce : 'Formation'
+  Lady Gaga : 'Million Reasons'
+  Billy Joel : 'Always a Woman'
+  Train : 'Drops of Jupiter'
+  Fleetwood Mac : 'Silver Springs'
+  Sam Smith : 'Too Good at Goodbyes'
+  Lizzo : 'Truth Hurts'
+  Robyn : 'Dancing on My Own'
+}
